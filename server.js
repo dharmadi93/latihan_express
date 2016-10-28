bf3c47de93579d0dfc789502ea0810383308f418
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-10-28 22:41:05 +0700

    selesai

--- server.js
+++ server.js
@@ -34,6 +34,56 @@ router.get('/books', (req, res) => {
     res.json(books)
 })
 
+router.get('/books/:id', (req, res) => {
+    const book = books.filter(book => {
+        return book.id === Number(req.params.id)
+    })[0]
+
+    if (!book) res.status(404).json({ 'message': 'no book found'})
+    res.statusCode(200).json(book)
+})
+
+router.post('/books', (req, res) => {
+    const book = {
+        id: Number(req.body.id),
+        name: req.body.name,
+        price: Number(req.body.price)
+    }
+    books.push(book)
+
+    res.json(book)
+})
+
+router.delete('/books/:id', (req, res) => {
+    // const od = req.params.id
+    const book = books.filter(book => {
+        return book.id === Number(req.params.id)
+    })[0]
+
+    if (!book) res.status(404).json({ 'message': 'no book found'})
+
+    books.splice(books.indexOf(book), 1)
+    res.status(200).json({ 'message': `Book ${req.params.id} has been deleted` })
+})
+
+router.put('/books/:id', (req, res) => {
+    // const od = req.params.id
+    const book = books.filter(book => {
+        return book.id === Number(req.params.id)
+    })[0]
+
+    if (!book) res.status(404).json({ 'message': 'no book found'})
+
+    Object.keys(req.body).forEach(key => {
+        book[key] = req.body[key]
+    })
+
+    books[books.indexOf(book)] = book
+
+    res.json(book)
+    // res.status(200).json({ 'message': `Book ${req.params.id} has been deleted` })
+})
+
 
 //register routes
 
